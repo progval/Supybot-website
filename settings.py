becade071e7d9755c485df6f34641cb899279671
Valentin Lorentz <progval@gmail.com>
2011-08-18 17:19:29 +0200

    settings.py: Move MEDIA_ROOT to localsettings.py

--- settings.py
+++ settings.py
@@ -35,7 +35,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/progval/workspace/Supybot/website/media/'
+from localsettings import MEDIA_ROOT
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
