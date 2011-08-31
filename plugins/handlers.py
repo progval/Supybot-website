d15fa8b2b1d5121b3e28c3d7c5ec8d3b04898ffb
Valentin Lorentz <progval@gmail.com>
2011-08-31 19:15:46 +0000

    API: Add ratings request.

--- plugins/handlers.py
+++ plugins/handlers.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
+from voting.models import Vote
+
 from piston.handler import AnonymousBaseHandler, BaseHandler
 from piston.resource import Resource
 
@@ -16,7 +18,7 @@ class PluginHandler(BaseHandler):
             return Plugin.objects.get(name=name, published=True)
 
 class PluginCommentHandler(BaseHandler):
-    allowed_methodes = ('GET',)
+    allowed_methods = ('GET',)
     model = PluginComment
 
     fields = ('text', 'user', ('key', ('name',)), 'created_date')
@@ -25,10 +27,20 @@ class PluginCommentHandler(BaseHandler):
         plugin = Plugin.objects.get(name=name, published=True)
         return PluginComment.objects.filter(key=plugin)
 
+class PluginRatingHandler(BaseHandler):
+    allowed_methods = ('GET',)
+
+    def read(self, request, name):
+        plugin = Plugin.objects.get(name=name, published=True)
+        score = Vote.objects.get_score(plugin)
+        return score
+
 plugin_handler = Resource(PluginHandler)
 plugin_comment_handler = Resource(PluginCommentHandler)
+plugin_rating_handler = Resource(PluginRatingHandler)
 urlpatterns = patterns('',
     url(r'list/', plugin_handler),
     url(r'view/(?P<name>.*)/', plugin_handler),
-    url('comments/(?P<name>.*)/', plugin_comment_handler)
+    url('comments/(?P<name>.*)/', plugin_comment_handler),
+    url('ratings/(?P<name>.*)/', plugin_rating_handler),
 )
