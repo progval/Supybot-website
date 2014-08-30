6d420a5d00ae507c852f178a1a75c62a89e674d5
Valentin Lorentz <progval@progval.net>
2014-08-30 12:28:31 +0000

    Use 8-character indexes for pastes.

--- dpaste/models.py
+++ dpaste/models.py
@@ -8,11 +8,11 @@ from django.utils.translation import ugettext_lazy as _
 from dpaste.highlight import LEXER_DEFAULT, pygmentize
 
 t = 'abcdefghijkmnopqrstuvwwxyzABCDEFGHIJKLOMNOPQRSTUVWXYZ1234567890'
-def generate_secret_id(length=4):
+def generate_secret_id(length=8):
     return ''.join([random.choice(t) for i in range(length)])
 
 class Snippet(models.Model):
-    secret_id = models.CharField(_(u'Secret ID'), max_length=4, blank=True)
+    secret_id = models.CharField(_(u'Secret ID'), max_length=8, blank=True)
     title = models.CharField(_(u'Title'), max_length=120, blank=True)
     author = models.CharField(_(u'Author'), max_length=30, blank=True)
     content = models.TextField(_(u'Content'), )
